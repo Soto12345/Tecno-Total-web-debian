2f40c20971ae72c75b004f045175b8b356426a78
soto12345 <diegoivansotoarreola@gmail.com>
2024-04-27 21:20:32 -0600

    se incluyo las librerias sabradav y TCPDF para generar pdfs

--- models/pay/ticket.php
+++ models/pay/ticket.php
@@ -1,5 +1,12 @@
 <?php
 require('../conf/connection.php');
+require_once('../../tcpdf/tcpdf.php');
+
+// $client = new Sabre\DAV\Client([
+//     'baseUri' => 'https://tu-servidor-webdav.com/', // URL base del servidor WebDAV
+//     'userName' => 'tu-usuario', // Nombre de usuario para la autenticación en WebDAV
+//     'password' => 'tu-contraseña', // Contraseña para la autenticación en WebDAV
+// ]);
 session_start();
 global $connection;
 $Client_ID = 'ARaJ_-lZ2bImQWLMJIrYbZu5_n1Vf0uF6ClGgqduTPpf3uRk3NqMH-BU94qh1DQG1a06xZb6fPQt7RDF';
@@ -34,6 +41,60 @@ $email = $object_answer_data->payer->email_address;
 $id_client = $object_answer_data->payer->payer_id;
 $name_user = $_SESSION['usuario'];
 
+// // Inicializar TCPDF
+// $pdf = new TCPDF();
+// $pdf->SetCreator(PDF_CREATOR);
+// $pdf->SetTitle('Comprobante de Compra');
+// $pdf->SetMargins(10, 10, 10);
+// $pdf->AddPage();
+// // Contenido del PDF
+// $html = '
+// <!DOCTYPE html>
+// <html lang="es">
+// <head>
+// <meta charset="UTF-8">
+// <meta name="viewport" content="width=device-width, initial-scale=1.0">
+// <title>Comprobante de Compra</title>
+// <style>
+//     body {
+//         font-family: Arial, sans-serif;
+//     }
+//     h1 {
+//         color: #333;
+//         font-size: 24px;
+//     }
+//     table {
+//         width: 100%;
+//         border-collapse: collapse;
+//     }
+//     th, td {
+//         border: 1px solid #ddd;
+//         padding: 8px;
+//         text-align: left;
+//     }
+//     th {
+//         background-color: #f2f2f2;
+//     }
+// </style>
+// </head>
+// <body>
+//     <h1>Comprobante de Compra</h1>
+//     <p><strong>ID de Ticket:</strong> ' . $id_ticket . '</p>
+//     <p><strong>Fecha:</strong> ' . $new_date . '</p>
+//     <p><strong>Status:</strong> ' . $status . '</p>
+//     <p><strong>Cliente:</strong> ' . $name_user . '</p>
+//     <p><strong>Correo Electrónico:</strong> ' . $email . '</p>
+//     <table>
+//         <thead>
+//             <tr>
+//                 <th>Producto</th>
+//                 <th>Cantidad</th>
+//                 <th>Precio Unitario</th>
+//                 <th>Total</th>
+//             </tr>
+//         </thead>
+//         <tbody>';
+// $total = 0;
 // Inserción en la tabla ticket
 foreach ($_SESSION['CART'] as $index => $product) {
     $product_name = $product['product_name'];
@@ -41,17 +102,49 @@ foreach ($_SESSION['CART'] as $index => $product) {
     $amount = $product['product_price'];
     $sql = "INSERT INTO ticket(id_ticket,id_client,nombre_usuario,nombre_producto,cantidad,monto,estado,fecha,email) VALUES('$id_ticket','$id_client','$name_user','$product_name','$product_stock','$amount','$status','$new_date','$email')";
     $connection->run_query($sql);
+    // $html .= '
+    //         <tr>
+    //             <td>' . $product_name . '</td>
+    //             <td>' . $product_stock . '</td>
+    //             <td>$' . $amount . '</td>
+    //             <td>$' . number_format($amount, 2) . '</td>
+    //         </tr>';
+    // $total += $amount * $product_stock;
 }
+// $html .= '
+//         </tbody>
+//         <tfoot>
+//             <tr>
+//                 <td colspan="3" align="right"><strong>Total:</strong></td>
+//                 <td>$' . number_format($total, 2) . '</td>
+//             </tr>
+//         </tfoot>
+//     </table>
+// </body>
+// </html>';
+// Agregar contenido al PDF y cerrar
+// $pdf->writeHTML($html, true, false, true, false, '');
+
+// Guardar el PDF en el servidor
+// $pdfFilePath = 'comprobantes/' . $id_ticket . '_comprobante.pdf'; // Ruta donde se guardará el archivo PDF
+// $pdf->Output($_SERVER['DOCUMENT_ROOT'] . '/' . $pdfFilePath, 'F');
+
+// Guardar la ruta del PDF en la base de datos u otro sistema de almacenamiento
+
+// Redirigir o mostrar un mensaje de éxito
+// echo "El comprobante se ha generado y almacenado correctamente en: " . $pdfFilePath;
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <title>TICKET</title>
 </head>
+
 <body>
     <div class="container mt-5">
         <div class="card">
@@ -112,4 +205,5 @@ foreach ($_SESSION['CART'] as $index => $product) {
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </body>
+
 </html>
